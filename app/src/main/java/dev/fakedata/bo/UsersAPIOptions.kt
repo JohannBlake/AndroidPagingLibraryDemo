75d3663bff8cb34f1ea89edfa30ff4fb0d6e652c
Johann Blake <johannblake@gmail.com>
2019-08-22 14:31:00 +0200

    Set image sizes to 100 px and sort by first name.

--- app/src/main/java/dev/fakedata/bo/UsersAPIOptions.kt
+++ app/src/main/java/dev/fakedata/bo/UsersAPIOptions.kt
@@ -4,7 +4,7 @@ data class UsersAPIOptions (
     var fromPredefinedList: Boolean = true,
     var startPos: Int = 0,
     var pageSize: Int = 20,
-    var sortDesc: Boolean = true,
-    var imageSize: Int = 200
+    var sortDesc: Boolean = false,
+    var imageSize: Int = 100
 
 )
